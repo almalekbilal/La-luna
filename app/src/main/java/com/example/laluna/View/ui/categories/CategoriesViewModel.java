04a4fa9ec01c52d4b5003ba0f090f01839a76e7a
AliMalla <alimal@student.chalmers.se>
2021-10-07 00:58:08 +0200

    Add a new java class CategoriesViewModel.java to work with CategoriesFragment

---
+++ app/src/main/java/com/example/laluna/View/ui/categories/CategoriesViewModel.java
@@ -0,0 +1,20 @@
+package com.example.laluna.View.ui.categories;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class CategoriesViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public CategoriesViewModel() {
+        mText = new MutableLiveData<>();
+       // mText.setValue("This is categories fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
+
