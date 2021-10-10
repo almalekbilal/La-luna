c43ba523a8c3bf62c65286ba507cfe9c0823aeb7
Bilal Almalek <bilalmal@student.chalmers.se>
2021-10-10 21:57:13 +0200

    insert temprary data to database

--- app/src/main/java/com/example/laluna/ui/MainActivity.java
+++ app/src/main/java/com/example/laluna/ui/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.laluna.ui;
 
 import android.os.Bundle;
 
+import com.example.laluna.Model.Category;
+import com.example.laluna.Model.DBHandler;
 import com.example.laluna.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -10,6 +12,8 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        tempInsertToDB();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -35,6 +40,59 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void tempInsertToDB(){
+
+        DBHandler db = new DBHandler(this);
+
+        this.deleteDatabase("laluna.db");
+
+
+        Category c1 = db.addCategory("Food",3000,R.drawable.categorycar,null,new Date(121,9,2));
+        Category c2 = db.addCategory("entertainment",2000,R.drawable.food,null,new Date(121,9,2));
+
+        Category c18 = db.addCategory("Car",1000,R.drawable.food,null,new Date(121,8,1));
+        Category c28 = db.addCategory("Drinks",3000,R.drawable.ic_home_black_24dp,null,new Date(121,8,1));
+
+
+        db.addExpense("Pizza", 80,new Date(),c1);
+        db.addExpense("KFC", 20,new Date(),c1);
+        db.addExpense("Pizza", 100,new Date(),c1);
+        db.addExpense("Cheese", 300,new Date(),c1);
+        db.addExpense("Tacos", 200,new Date(),c1);
+
+        //does not count
+
+
+        db.addExpense("Disco", 200,new Date(),c2);
+        db.addExpense("zip line", 500,new Date(),c2);
+
+        db.addExpense("Window", 80,new Date(121,8,2),c18);
+        db.addExpense("Gear", 20,new Date(121,8,2),c18);
+        db.addExpense("Pip", 100,new Date(121,8,2),c18);
+        db.addExpense("Oil", 300,new Date(121,8,2),c18);
+        db.addExpense("Oil", 200,new Date(121,8,2),c18);
+
+        //does not count
+
+        db.addExpense("Vodka", 80,new Date(121,8,2),c28);
+        db.addExpense("Bear", 500,new Date(121,8,2),c28);
+        db.addExpense("Coca cola", 200,new Date(121,8,2),c28);
+        db.addExpense("Pepsi", 500,new Date(121,8,2),c28);
+
+        db.addExpense("Fanta", 700,new Date(),c28);
+        db.addExpense("whiskey", 300,new Date(),c18);
+
+        db.addExpense("Oil", 80,new Date(121,8,2),c18);
+        db.addExpense("Motor", 80,new Date(121,8,2),c18);
+        db.addExpense("CS go", 80,new Date(),c2);
+        db.addExpense("Liseberg", 500,new Date(),c2);
+        db.addExpense("Burger", 80,new Date(),c1);
+        db.addExpense("Fish", 80,new Date(),c1);
+
+        db.setCategoriesPreviousLimits(new Date(121,8,1));
+
+
+    }
 
 
 }
