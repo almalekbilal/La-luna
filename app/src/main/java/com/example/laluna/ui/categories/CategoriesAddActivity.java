6cd71ffdab90b324cb03172bff4c5960dd60e8d6
Ali <alialk@student.chalmers.se>
2021-10-13 21:32:38 +0200

    Layout for add category fragment is done! Also, the java class file for it is added. (Not done yet)

---
+++ app/src/main/java/com/example/laluna/ui/categories/CategoriesAddActivity.java
@@ -0,0 +1,18 @@
+package com.example.laluna.ui.categories;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.laluna.R;
+
+public class CategoriesAddActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_add);
+    }
+}
