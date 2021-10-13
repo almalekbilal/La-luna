fa3803744e3f39ef3ec783ac2d7ecacd37784db8
Ali <alialk@student.chalmers.se>
2021-10-13 21:33:58 +0200

    Layout for edit category fragment is done! Also, the java class file for it is added. (Not done yet)

---
+++ app/src/main/java/com/example/laluna/ui/categories/CategoriesEditActivity.java
@@ -0,0 +1,18 @@
+package com.example.laluna.ui.categories;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.laluna.R;
+
+public class CategoriesEditActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_edit);
+    }
+}
