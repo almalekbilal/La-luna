daa9ccb3c9b96916feef7da7aed9492c4bc665ce
AliMalla <alimal@student.chalmers.se>
2021-10-19 12:11:15 +0200

    Break down getView method into small parts (functional decomposition)

--- app/src/main/java/com/example/laluna/ui/analysis/categoryExpensesActivity/ListViewAdapter.java
+++ app/src/main/java/com/example/laluna/ui/analysis/categoryExpensesActivity/ListViewAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,18 +20,24 @@ public class ListViewAdapter extends ArrayAdapter {
 
     private List<Expense> expenses;
 
-    private int id;
+    private int categoryId;
+
+    private Context context;
+    private LayoutInflater layoutInflater;
 
 
-    Context context;
-    LayoutInflater layoutInflater;
-    public ListViewAdapter(List<Expense> expenses, Context context, int id) {
+
+    private TextView txv_expensePrice;
+    private TextView txv_expenseName;
+    private ImageView img_categoryImage;
+    private TextView txv_dateExpense;
+
+
+    public ListViewAdapter(List<Expense> expenses, Context context, int categoryId) {
         super(context, R.layout.custom_expense_view,expenses);
         this.expenses= expenses;
         this.context = context;
-        this.id = id;
-
-
+        this.categoryId = categoryId;
     }
 
 
@@ -46,24 +53,33 @@ public class ListViewAdapter extends ArrayAdapter {
             convertView = layoutInflater.inflate(R.layout.custom_expense_view,null);
         }
 
+        initExpenseInformation(convertView);
 
-        TextView txv_expensePrice = convertView.findViewById(R.id.txv_expensePrice);
-        TextView txv_expenseName = convertView.findViewById(R.id.txv_expenseName);
-        ImageView img_categoryImage = convertView.findViewById(R.id.img_categoryImage);
-        TextView txv_dateExpense = convertView.findViewById(R.id.txv_dateExpense);
+        showExpenseInformation(position);
+
+        return convertView;
+    }
 
 
-        if(expenses.get(position).get_category().get_id() == id){
+    private void initExpenseInformation(View convertView){
+        txv_expensePrice = convertView.findViewById(R.id.txv_expensePrice);
+        txv_expenseName = convertView.findViewById(R.id.txv_expenseName);
+        img_categoryImage = convertView.findViewById(R.id.img_categoryImage);
+        txv_dateExpense = convertView.findViewById(R.id.txv_dateExpense);
+    }
+
+
+    private void showExpenseInformation(int position){
+        if(checkCategoryId(position)){
             txv_expensePrice.setText(Integer.toString(expenses.get(position).get_value()));
             txv_expenseName.setText(expenses.get(position).get_name());
             img_categoryImage.setImageResource(expenses.get(position).get_category().get_pictureName());
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
             txv_dateExpense.setText(sdf.format(expenses.get(position).get_date()));
         }
+    }
 
-
-
-
-        return convertView;
+    private boolean checkCategoryId(int position){
+        return expenses.get(position).get_category().get_id() == categoryId;
     }
 }
