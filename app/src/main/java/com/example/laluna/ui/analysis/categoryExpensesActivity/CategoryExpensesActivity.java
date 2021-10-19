ad76ea202c3d51cefdbe15da9349307332443d9c
AliMalla <alimal@student.chalmers.se>
2021-10-19 13:09:50 +0200

    Write a javadoc and some comments

--- app/src/main/java/com/example/laluna/ui/analysis/categoryExpensesActivity/CategoryExpensesActivity.java
+++ app/src/main/java/com/example/laluna/ui/analysis/categoryExpensesActivity/CategoryExpensesActivity.java
@@ -24,15 +24,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
+/**
+ * This class is responsible for showing information about the category and its expenses to the user
+ * @author Ali Malla
+ */
 public class CategoryExpensesActivity extends AppCompatActivity {
 
 
-    private double spent;
-    private double budget;
+    private double spent;          // category spent money
+    private double budget;         // category budget
 
     private AnalysisViewModel analysisViewModel;
 
-    private PieChart clickedCategoryPieCh;
+    private PieChart clickedCategoryPieCh;   // category pie chart to show spent and left money
+
     private TextView categoryNameAndBudget;
     private TextView categorySpent;
     private ImageButton backButton;
@@ -40,11 +45,15 @@ public class CategoryExpensesActivity extends AppCompatActivity {
 
     private Intent intent;
 
-    private int categoryId;
+    private int categoryId;  // We use category id to send each expense into its category
     private int categoryYear;
     private int categoryMonth;
 
 
+    /**
+     * This method show all things in the activity
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,7 +66,9 @@ public class CategoryExpensesActivity extends AppCompatActivity {
     }
 
 
-
+    /**
+     * this method gets all the items that will shows in the activity
+     */
     private void showAllCategoryInformation(){
         spent = getSpent();
         budget = getBudget();
@@ -77,6 +88,9 @@ public class CategoryExpensesActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * This method initializes most of the class's attributes
+     */
     private void init(){
         setContentView(R.layout.activity_category_expenses);
 
@@ -93,6 +107,10 @@ public class CategoryExpensesActivity extends AppCompatActivity {
 
     }
 
+
+    /**
+     * This method responsible to set category name and budget
+     */
     private void showCategoryNameAndBudget(){
         String selectedCategoryName = intent.getStringExtra("name");
         clickedCategoryPieCh.setCenterText(selectedCategoryName);   //Category name in pie chart
@@ -101,21 +119,36 @@ public class CategoryExpensesActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * A method to get category spent by intent
+     * @return category spent
+     */
     private double getSpent(){
         return intent.getDoubleExtra("spent",0);
     }
 
+    /**
+     * A method to get category budget by intent
+     * @return category budget
+     */
     private double getBudget(){
         return intent.getDoubleExtra("budget",0);
     }
 
 
+    /**
+     * This method to set category spent money in the activity
+     */
     private void showCategorySpentMoney(){
         String spentToString = Double.toString(getSpent());
         categorySpent.setText(spentToString + " KR");
     }
 
 
+    /**
+     * This method is responsible to back from activity to Analysis fragment when the user click on
+     * back button
+     */
     private void backButtonToAnalysisFragment(){
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -125,24 +158,46 @@ public class CategoryExpensesActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * A method to get category id (selected category) by intent
+     * @return category id
+     */
     private int getSelectedCategoryId(){
         return intent.getIntExtra("id",0);
     }
 
+    /**
+     * A method to get category year (selected category) by intent
+     * @return category year
+     */
     private int getSelectedCategoryYear(){
         return intent.getIntExtra("categoryYear",5);
     }
 
+    /**
+     * A method to get category month (selected category) by intent
+     * @return category month
+     */
     private int getSelectedCategoryMonth(){
         return intent.getIntExtra("categoryMonth",9);
     }
 
 
+    /**
+     * A method to get the expenses of a specific category
+     * @param categoryId Category id for the specific category
+     * @param categoryYear Category year for the specific category
+     * @param categoryMonth Category month for the specific category
+     */
     private void getCategoryExpenses(int categoryId, int categoryYear, int categoryMonth){
         analysisViewModel.updateCategoryExpenses(categoryId, categoryYear, categoryMonth);
     }
 
 
+    /**
+     * This method is responsible to add all category expenses to the listView in activity by
+     * listViewAdapter
+     */
     private void showCategoryExpensesInListView(){
         final ListViewAdapter listAdapter = new ListViewAdapter
                 (new ArrayList<Expense>(), this, categoryId);
@@ -163,6 +218,11 @@ public class CategoryExpensesActivity extends AppCompatActivity {
 
     }
 
+
+    /**
+     * A method to make a pie chart according to specific criteria for category to show spent category money
+     * @param piechart category pie chart that the method will work with
+     */
     private void makeCategoryPieCh(PieChart piechart) {
         piechart.setUsePercentValues(true);
 
