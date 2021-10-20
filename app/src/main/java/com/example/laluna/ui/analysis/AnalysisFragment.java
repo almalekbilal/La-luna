625d5deffb86600ae177af6a11931b9d6cd48327
AliMalla <alimal@student.chalmers.se>
2021-10-20 11:25:53 +0200

    Break down onCreateView method into small parts (functional decomposition) ,clean the code and write some javadoc to the new methods

--- app/src/main/java/com/example/laluna/ui/analysis/AnalysisFragment.java
+++ app/src/main/java/com/example/laluna/ui/analysis/AnalysisFragment.java
@@ -38,8 +38,19 @@ public class AnalysisFragment extends Fragment {
 
     private AnalysisViewModel analysisViewModel;
     private GridViewAdapter gridViewAdapter;
-    private double total = 300, spent= 150;
 
+    private double total , spent;
+
+
+    private GridView gridViewAnalysis;
+    private TextView dateText;
+    private TextView budgetText;
+    private PieChart piechart;
+    private TextView leftArrow;
+    private TextView rightArrow;
+
+    private View root;
+    private Intent intent;
 
     /**
      * This method creates the view and handle the widgets in it
@@ -48,63 +59,94 @@ public class AnalysisFragment extends Fragment {
      * @param savedInstanceState
      * @return
      */
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        analysisViewModel =
-                ViewModelProviders.of(this).get(AnalysisViewModel.class);
-        final View root = inflater.inflate(R.layout.fragment_analysis, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        final GridView gridViewAnalysis = root.findViewById(R.id.gridViewAnalysis);
+        init(inflater,container);
+        makePie(piechart);
+        openCategoryExpensesActivity();
+        updateCategories();
+        updateBigPieChart();
+        showTotalBudget();
+        updateDate();
+        updateViewWhenClickOnLeftArrow();
+        updateViewWhenClickOnRightArrow();
 
-        final TextView dateText = root.findViewById(R.id.txv_date);
-        final TextView budgetText = root.findViewById(R.id.txv_budgetAnalysis);
+        return root;
+    }
 
+
+
+    /**
+     * This method initializes the class's attributes
+     * @param inflater
+     * @param container
+     */
+    private void init(LayoutInflater inflater, ViewGroup container){
+        analysisViewModel =  ViewModelProviders.of(this).get(AnalysisViewModel.class);
+        root = inflater.inflate(R.layout.fragment_analysis, container, false);
+
+        gridViewAnalysis = root.findViewById(R.id.gridViewAnalysis);
+        dateText = root.findViewById(R.id.txv_date);
+        budgetText = root.findViewById(R.id.txv_budgetAnalysis);
+        leftArrow = root.findViewById(R.id.txv_leftArrow);
+        rightArrow = root.findViewById(R.id.txv_rightArrow);
+        piechart = root.findViewById(R.id.pc_categorySpent);
 
         gridViewAdapter = new GridViewAdapter(new ArrayList<CategoryWithMoney>(),root.getContext());
-
 
         gridViewAnalysis.setAdapter(gridViewAdapter);
 
         analysisViewModel.init(getContext());
 
+        intent = new Intent(root.getContext(), CategoryExpensesActivity.class);
 
-        final PieChart piechart = root.findViewById(R.id.pc_categorySpent);
+    }
 
-        makePie(piechart);
-
-
-
-
-        /////// To open a new activity from analysis page.
+    /**
+     * This method is responsible for open a new activity (category expenses) when the user
+     * click on a category in grid view list
+     */
+    private void openCategoryExpensesActivity(){
         gridViewAnalysis.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(root.getContext(), CategoryExpensesActivity.class);
-
-                String selectedName = analysisViewModel.getCategories().getValue().get(i).category.get_name();
-                double selectedSpent = analysisViewModel.getCategories().getValue().get(i).spent;
-                double selectedBudget = analysisViewModel.getCategories().getValue().get(i).limit;
-                int selectedCategoryId= analysisViewModel.getCategories().getValue().get(i).category.get_id();
-                int selectedCategoryMonth = analysisViewModel.getDate().getValue().getMonth();
-                int selectedCategoryYear = analysisViewModel.getDate().getValue().getYear();
-
-                intent.putExtra("spent",selectedSpent);
-                intent.putExtra("budget",selectedBudget);
-                intent.putExtra("name",selectedName);
-                intent.putExtra("id",selectedCategoryId);
-                intent.putExtra("categoryMonth",selectedCategoryMonth);
-                intent.putExtra("categoryYear",selectedCategoryYear);
-
+                sendSelectedCategoryInformationToActivity(i);
 
                 startActivity(intent);
             }
         });
+    }
 
 
+    /**
+     * A method to send a selected category information to a new activity that opens when the user
+     * click on the category in analysis fragment.
+     * @param i category position
+     */
+    private void sendSelectedCategoryInformationToActivity(int i){
 
 
+        String selectedName = analysisViewModel.getCategories().getValue().get(i).category.get_name();
+        double selectedSpent = analysisViewModel.getCategories().getValue().get(i).spent;
+        double selectedBudget = analysisViewModel.getCategories().getValue().get(i).limit;
+        int selectedCategoryId= analysisViewModel.getCategories().getValue().get(i).category.get_id();
+        int selectedCategoryMonth = analysisViewModel.getDate().getValue().getMonth();
+        int selectedCategoryYear = analysisViewModel.getDate().getValue().getYear();
 
 
+        intent.putExtra("spent",selectedSpent);
+        intent.putExtra("budget",selectedBudget);
+        intent.putExtra("name",selectedName);
+        intent.putExtra("id",selectedCategoryId);
+        intent.putExtra("categoryMonth",selectedCategoryMonth);
+        intent.putExtra("categoryYear",selectedCategoryYear);
+    }
+
+
+    /**
+     * A method to update categories in grid view.
+     */
+    private void updateCategories() {
         analysisViewModel.getCategories().observe(this, new Observer<List<CategoryWithMoney>>() {
             @Override
             public void onChanged(List<CategoryWithMoney> categoryWithMonies) {
@@ -113,9 +155,13 @@ public class AnalysisFragment extends Fragment {
 
             }
         });
+    }
 
 
-
+    /**
+     * A method to update the big pie chart
+     */
+    private void updateBigPieChart(){
         analysisViewModel.getTotalAndSpent().observe(this, new Observer<List<Integer>>() {
             @Override
             public void onChanged(List<Integer> integers) {
@@ -129,34 +175,63 @@ public class AnalysisFragment extends Fragment {
                 makePie(piechart);
             }
         });
+    }
 
+
+    /**
+     * A method to show the total budget in view
+     */
+    private void showTotalBudget(){
+        budgetText.setText("Your budget : " + total);
+    }
+
+
+    /**
+     * A method to update the date
+     */
+    private void updateDate(){
         analysisViewModel.getDate().observe(this, new Observer<Date>() {
             @Override
             public void onChanged(Date date) {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                dateText.setText(sdf.format(date));
+                showDate(date);
             }
         });
+    }
 
-        TextView left = root.findViewById(R.id.txv_leftArrow);
-        TextView right = root.findViewById(R.id.txv_rightArrow);
 
-        left.setOnClickListener(new View.OnClickListener() {
+    /**
+     * A method to show the date in view
+     * @param date the date that will be displayed
+     */
+    private void showDate(Date date){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        dateText.setText(sdf.format(date));
+    }
+
+
+    /**
+     * A method to update view when the user click on left arrow (previous month)
+     */
+    private void updateViewWhenClickOnLeftArrow(){
+        leftArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 analysisViewModel.leftArrowClick();
             }
         });
+    }
 
-        right.setOnClickListener(new View.OnClickListener() {
+
+    /**
+     * A method to update view when the user click on right arrow (next month)
+     */
+    private void updateViewWhenClickOnRightArrow(){
+        rightArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 analysisViewModel.rightArrowClick();
             }
         });
-
-
-        return root;
     }
 
     /**
