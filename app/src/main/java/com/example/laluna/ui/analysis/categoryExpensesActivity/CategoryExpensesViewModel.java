88282cf82652ecd28f01b2a4afc2e5c62dbdbc29
AliMalla <alimal@student.chalmers.se>
2021-10-20 12:11:27 +0200

    write javadoc to the new java class

--- app/src/main/java/com/example/laluna/ui/analysis/categoryExpensesActivity/CategoryExpensesViewModel.java
+++ app/src/main/java/com/example/laluna/ui/analysis/categoryExpensesActivity/CategoryExpensesViewModel.java
@@ -13,32 +13,38 @@ import java.util.ArrayList;
 
 import java.util.List;
 
+/**
+ * CategoryExpensesViewModel class that is responsible for the communication with the
+ * data base handler and the logic behind the scene
+ * @author Ali Malla
+ */
 public class CategoryExpensesViewModel extends ViewModel {
 
     private DBHandler dbHandler;
     private MutableLiveData<List<Expense>> categoryExpenses;
 
 
-
-
-
+    /**
+     * This method initializes the class's attributes
+     * @param context the view context that will work with this class
+     */
     public void init(Context context){
         dbHandler = new DBHandler(context);
 
         categoryExpenses = new MutableLiveData<>();
-
     }
 
 
-
-
+    /**
+     *  The method gets the expenses of a specific category and sends them to the view
+     * @param categoryId  id of the specific category
+     * @param year Expenses year
+     * @param month Expenses month
+     */
     public void updateCategoryExpenses(int categoryId,int year,int month){
-
         List<Expense> expenses = new ArrayList<>();
 
         List<Expense> categoryEx = dbHandler.getCategoryExpense(categoryId);
-
-
 
         for(Expense expense: categoryEx){
             if(expense.get_date().getYear() == year &&
@@ -50,9 +56,13 @@ public class CategoryExpensesViewModel extends ViewModel {
     }
 
 
-
-
+    /**
+     * The method returns the specific category expenses as a LiveData object
+     * Its responsible for the communication with the view
+     * @return category expenses as a liveData object
+     */
     public LiveData<List<Expense>> getCategoryExpenses() {
         return categoryExpenses;
     }
+
 }
