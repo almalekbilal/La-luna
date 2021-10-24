644469e79598f5b9b6f15cd839fe8fc86faae139
Ali <alialk@student.chalmers.se>
2021-10-24 21:39:18 +0200

    test

--- app/src/androidTest/java/com/example/laluna/DatabaseClassTest.java
+++ app/src/androidTest/java/com/example/laluna/DatabaseClassTest.java
@@ -516,62 +516,8 @@ public class DatabaseClassTest {
         Category health = db.addCategory("Health",1000,R.drawable.health,null,new Date(121,7,1));
         Category other = db.addCategory("Other",3000,R.drawable.other,null,new Date(121,7,1));
 
-
         assertEquals(true, db.thereIsCategories(new Date(121,7,1)));
     }
-
-
-    /*@Test
-    public void ViewThings(){
-        Category c1 = db.addCategory("Food",3000,R.drawable.categorycar,null,new Date(121,9,2));
-        Category c2 = db.addCategory("entertainment",2000,R.drawable.food,null,new Date(121,9,2));
-
-        Category c18 = db.addCategory("Car",1000,R.drawable.food,null,new Date(121,8,1));
-        Category c28 = db.addCategory("Drinks",3000,R.drawable.ic_home_black_24dp,null,new Date(121,8,1));
-
-
-        db.addExpense("Pizza", 80,new Date(),c1);
-        db.addExpense("KFC", 20,new Date(),c1);
-        db.addExpense("Pizza", 100,new Date(),c1);
-        db.addExpense("Cheese", 300,new Date(),c1);
-        db.addExpense("Tacos", 200,new Date(),c1);
-
-        //does not count
-
-
-        db.addExpense("Disco", 200,new Date(),c2);
-        db.addExpense("zip line", 500,new Date(),c2);
-
-        db.addExpense("Window", 80,new Date(121,8,2),c18);
-        db.addExpense("Gear", 20,new Date(121,8,2),c18);
-        db.addExpense("Pip", 100,new Date(121,8,2),c18);
-        db.addExpense("Oil", 300,new Date(121,8,2),c18);
-        db.addExpense("Oil", 200,new Date(121,8,2),c18);
-
-        //does not count
-
-        db.addExpense("Vodka", 80,new Date(121,8,2),c28);
-        db.addExpense("Bear", 500,new Date(121,8,2),c28);
-        db.addExpense("Coca cola", 200,new Date(121,8,2),c28);
-        db.addExpense("Pepsi", 500,new Date(121,8,2),c28);
-
-        db.addExpense("Fanta", 700,new Date(),c28);
-        db.addExpense("whiskey", 300,new Date(),c18);
-
-        db.addExpense("Oil", 80,new Date(121,8,2),c18);
-        db.addExpense("Motor", 80,new Date(121,8,2),c18);
-        db.addExpense("CS go", 80,new Date(),c2);
-        db.addExpense("Liseberg", 500,new Date(),c2);
-        db.addExpense("Burger", 80,new Date(),c1);
-        db.addExpense("Fish", 80,new Date(),c1);
-
-        db.setCategoriesPreviousLimits(new Date(121,8,1));
-
-        assertEquals(3000,3000);
-    }
-
-     */
-
 }
 
 
